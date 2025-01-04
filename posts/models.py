9407a62ef212ed84d7466e85809ad49fa0c333c2
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2025-01-04 23:13:54 +0545

    File uploads with image field

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 from django.urls import reverse
+import os
 
 # Create your models here.
+def upload_location(instance, filename): # where images are actually uploaded to 
+    return os.path.join(str(instance.id), filename)
+    # return "%s/%s" %(instance.id, filename)
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
-    image = models.FileField(null=True)
+    image = models.ImageField(upload_to=upload_location, 
+            null=True, 
+            blank=True, 
+            width_field="width_field", 
+            height_field="height_field")
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
