bc4d03890c3462a465537a16230eccd57b36a829
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2025-01-09 23:28:15 +0545

    Pagedown for stack overflow style markdown

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts'
+    # third party library
+    'pagedown',
+    # local
+    'posts',
 ]
 
 MIDDLEWARE = [
