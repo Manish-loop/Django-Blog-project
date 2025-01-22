2973c3918bd85b1f6b2d9dd249b8d045aca20e6e
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2025-01-22 17:03:34 +0545

    Serializer Method field in serilaizers

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField
+from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField, SerializerMethodField
 
 from posts.models import Post
 
@@ -11,6 +11,7 @@ class PostCreateUpdateSerializer(ModelSerializer):
             'publish',
         ]
 
+
 post_detail_url = HyperlinkedIdentityField(
         view_name='posts-api:detail',
         lookup_field='slug'
@@ -18,19 +19,42 @@ post_detail_url = HyperlinkedIdentityField(
 
 class PostDetailSerializer(ModelSerializer):
     url = post_detail_url
+    user = SerializerMethodField()
+    image = SerializerMethodField()
+    html = SerializerMethodField()
+
     class Meta:
         model = Post
         fields = [
             'url',
             'id',
+            'user',
             'title',
             'slug',
             'content',
+            'html',
             'publish',
+            'image'
         ]
+    
+
+    def get_html(self,obj):
+        return obj.get_html()
+
+    def get_user(self, obj):
+        return str(obj.user.username)
+    
+    def get_image(self, obj):
+        try:
+            image = obj.image.url
+        except:
+            image = None
+        return image
+
 
 class PostListSerializer(ModelSerializer):
     url = post_detail_url
+    user = SerializerMethodField()
    
     class Meta:
         model = Post
@@ -42,5 +66,8 @@ class PostListSerializer(ModelSerializer):
             'content',
             'publish',
         ]
+
+    def get_user(self, obj):
+        return str(obj.user.username)
 
 
