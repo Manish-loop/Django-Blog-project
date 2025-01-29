38533239c2ec940a31c3b723bebf3e15aa3eee0e
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2025-01-29 22:55:25 +0545

    User login api validation

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,16 @@
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 
 
-from rest_framework.serializers import EmailField, HyperlinkedIdentityField,ModelSerializer, SerializerMethodField,  ValidationError
+from rest_framework.serializers import (
+    CharField,
+    EmailField, 
+    HyperlinkedIdentityField,
+    ModelSerializer, 
+    SerializerMethodField, 
+    ValidationError
+    )
 
 
 User = get_user_model()
@@ -66,3 +74,46 @@ class UserCreateSerializer(ModelSerializer):
         user_obj.save()
         return validated_data
 
+
+class UserLoginSerializer(ModelSerializer):
+    token = CharField(allow_blank=True, read_only=True)
+    username = CharField(required=False, allow_blank=True)
+    email = EmailField(label='Email Address', required=False, allow_blank=True)
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password',
+            'token',
+            
+        ]
+        extra_kwargs = {"password":
+                              {"write_only": True}
+                        }
+    
+    def validate(self, data):
+        user_obj = None
+        email = data.get("email", None)
+        username = data.get("username", None)
+        password = data["password"]
+        if not email and not username:
+            raise ValidationError("A username or email is required to login.")
+        
+        user = User.objects.filter(
+               Q(email=email) |
+               Q(username=username)
+            ).distinct()
+        user = user.exclude(email__isnull=True).exclude(email__iexact='')
+        if user.exists() and user.count() == 1:
+            user_obj = user.first()
+        else:
+            raise ValidationError("This username/email is not valid.")
+        
+        if user_obj:
+            if not user_obj.check_password(password):
+                raise ValidationError("Incorrect credentials please try again.")
+            
+        data["token"] = "SOME RANDOM TOKEN"
+        return data
--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
-    UserCreateAPIView 
+    UserCreateAPIView ,
+    UserLoginAPIView
     )
 
 app_name = 'users'
 
 urlpatterns = [    
-    path('register/', UserCreateAPIView .as_view(), name='register'), 
+    path('login/', UserLoginAPIView.as_view(), name='login'), 
+    path('register/', UserCreateAPIView.as_view(), name='register'), 
 ]
